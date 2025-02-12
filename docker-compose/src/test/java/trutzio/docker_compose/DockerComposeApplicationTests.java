bd7c1d4f634de580d89d99a669e9ab6e49ed7bd8
Christian Trutz <info@trutz-software-consulting.de>
2025-02-12 16:41:21 +0100

    docker-compose

---
+++ docker-compose/src/test/java/trutzio/docker_compose/DockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package trutzio.docker_compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
